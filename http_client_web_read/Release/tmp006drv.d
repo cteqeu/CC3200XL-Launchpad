54165baea0a4b3f579371dbb1a4871b0562d7b55
Hogeschool PXL - Electronics-ICT <vincent.claes@pxl.be>
2017-11-10 14:14:51 +0100

    http client web read
    basic http client website read by use of GET

---
+++ http_client_web_read/Release/tmp006drv.d
@@ -0,0 +1,33 @@
+# FIXED
+
+tmp006drv.obj: ../tmp006drv.c
+tmp006drv.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/stdio.h
+tmp006drv.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/linkage.h
+tmp006drv.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/stdarg.h
+tmp006drv.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/math.h
+tmp006drv.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/ymath.h
+tmp006drv.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/yvals.h
+tmp006drv.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/stdarg.h
+tmp006drv.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/_lock.h
+tmp006drv.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/float.h
+tmp006drv.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/_math_config.h
+tmp006drv.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/errno.h
+tmp006drv.obj: ../tmp006drv.h
+tmp006drv.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/i2c_if.h
+tmp006drv.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/uart_if.h
+
+../tmp006drv.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/stdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/stdarg.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/math.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/ymath.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/yvals.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/stdarg.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/_lock.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/float.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/_math_config.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_5.2.9/include/errno.h: 
+../tmp006drv.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/i2c_if.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/uart_if.h: 
